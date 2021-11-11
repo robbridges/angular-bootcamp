02debd139906fce5ce88a83ab980098c3888a140
robbridges <robbridges@users.noreply.github.com>
2021-11-10 18:43:08 -0800

    Prettifying the angular project some more, Having stepped away from Angular for a moment I realize I missed it.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { MatInputModule} from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import {PostCreateComponent} from './posts/posts-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
@@ -21,6 +24,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatInputModule,
     MatCardModule,
     MatButtonModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
