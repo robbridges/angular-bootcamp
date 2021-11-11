cd37d93dacaee060f6aeeaf25afa59262709cd12
robbridges <robbridges@users.noreply.github.com>
2021-11-10 18:28:30 -0800

    Sprucing up angular, making it look half-way presentable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import {PostCreateComponent} from './posts/posts-create/post-create.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,6 +17,10 @@ import {PostCreateComponent} from './posts/posts-create/post-create.component';
   imports: [
     BrowserModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
